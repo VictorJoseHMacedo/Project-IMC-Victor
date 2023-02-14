8177235d475d2f6434c8aff5152fd19d72c72a8b
VictorJoseHMacedo <bitojose8@gmail.com>
2023-02-14 15:48:14 -0300

    Atualizações JS

--- js/index.js
+++ js/index.js
@@ -6,17 +6,15 @@ function formatImc(value){
 
 function tabelaIMC (value) {
     if (value < 18.5) {
-        return 'Abaixo do peso normal'
-    } else if (value <= 18.5 && value <= 24.9){
-        return 'Peso normal'
-    } else if (value <= 25.0 && value <= 29.9){
-        return 'Excesso de peso'
-    } else if (value <= 30.0 && value <= 34.9){
-        return 'Obesidade classe 1'
-    } else if (value <= 35.0 && value <= 39.9){
-        return 'Obesidade classe 2'
-    } else if (value >= 40.0){
-        return 'Obesidade classe 3'
+        return 'Magreza'
+    } else if (value >= 18.5 && value <= 24.9){
+        return 'Normal'
+    } else if (value >= 25.0 && value <= 29.9){
+        return 'Sobrepeso'
+    } else if (value >= 30.0 && value <= 34.9){
+        return 'Obesidade'
+    } else if (value > 40.0){
+        return 'Obesidade grave'
     }
 }
 
